5d616075c41c7fc3c1b3f29868794507cac2d3df
Cerise De Wannemaeker <c.wannemaeker@interface3.be>
2019-10-17 15:31:01 +0200

    test pour mon projet de site cv, début du JS/Jquery

---
+++ src/index.js
@@ -0,0 +1,47 @@
+import $ from 'jquery';
+
+const btn = $('#btn-portfolio');
+btn.on('click', () => {
+  const d = $('#design');
+  const p = $('#programmation');
+  if (d.css('display') === 'block') {
+    d.css({ display: 'none' });
+    p.css({ display: 'block' });
+    btn.text('Programmation');
+  } else {
+    d.css({ display: 'block' });
+    p.css({ display: 'none' });
+    btn.text('Design');
+  }
+});
+
+// btn menu
+const btnportfolio = $('a.portfolio');
+const btnabout = $('a.about');
+const btncv = $('a.cv');
+const btncontact = $('a.contact');
+
+function menuBtn(x) {
+  const str = x.target.attributes.class.nodeValue;
+  const y = $(`#${str}`);
+
+  $('header div ul li a').css({
+    backgroundColor: '#2b2934',
+    color: 'white',
+  });
+  $('.sectionMain').css({ display: 'none' });
+
+  $(x.target).css({
+    backgroundColor: 'white',
+    color: '#2b2934',
+  });
+  y.css('display', 'block');
+}
+
+const x = $('.sectionMain');
+
+
+btnabout.on('click', (e) => { menuBtn(e); });
+btncontact.on('click', (e) => { menuBtn(e); });
+btnportfolio.on('click', (e) => { menuBtn(e); });
+btncv.on('click', (e) => { menuBtn(e); });
